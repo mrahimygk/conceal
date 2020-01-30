37d5ca93f0f5331a84a20880ab33ca8bdf3da18c
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-01-30 18:03:18 +0330

    +test for wave manipulation

---
+++ app/src/test/java/ir/mrahimy/conceal/WaveManipulationUnitTest.kt
@@ -0,0 +1,39 @@
+package ir.mrahimy.conceal
+
+import ir.mrahimy.conceal.data.absolute
+import ir.mrahimy.conceal.data.mapToUniformDouble
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class WaveManipulationUnitTest {
+
+    val data = LongArray(11) {
+        (it * 10).toLong()
+    }
+
+    val negs = LongArray(11) {
+        (it * -10).toLong()
+    }
+
+    @Test
+    fun mapToUniformDouble() {
+        val double = data.mapToUniformDouble()
+        assert(100L == data.max())
+        assert(double[0] == 0.0)
+        assert(double[1] == data[1].toDouble() / (data.max()?.toDouble() ?: 0.0))
+        assert(double[1] == 0.1)
+        assert(double.max() ?: 0.0 <= 1.0)
+    }
+
+    @Test
+    fun mapToUniformDoubleNegative() {
+        val data = negs
+        val double = data.mapToUniformDouble()
+        assert(100L == data.absolute().max())
+        assert(double[0] == 0.0)
+        assert(double[1] == data[1].toDouble() / (data.absolute().max()?.toDouble() ?: 0.0))
+        assert(double[1] == -0.1)
+        assert(double.max() ?: 0.0 <= 1.0)
+    }
+}
