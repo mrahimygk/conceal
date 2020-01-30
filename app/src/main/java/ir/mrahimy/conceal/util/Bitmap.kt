ebd3d398ad7ac97b35e8fe754daa7118fb0872f5
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-01-30 17:43:09 +0330

    +bitmap utils to work with rgb values

---
+++ app/src/main/java/ir/mrahimy/conceal/util/Bitmap.kt
@@ -0,0 +1,22 @@
+package ir.mrahimy.conceal.util
+
+import android.graphics.Bitmap
+
+fun Bitmap.getRgb(x: Int, y: Int) : Rgb = this.getPixel(x, y).toRgb()
+
+fun Bitmap.getRgbArray(): List<Rgb> {
+    val rgb = mutableListOf<Rgb>()
+    (0 until width).forEach { x ->
+        (0 until height).forEach { y ->
+            rgb.add(getRgb(x, y))
+        }
+    }
+    return rgb
+}
+
+fun List<Rgb>.remove3Lsb(): List<Rgb> = map {
+    val r = it.r.removeLsBits(3)
+    val g = it.g.removeLsBits(3)
+    val b = it.b.removeLsBits(3)
+    Rgb(r, g, b)
+}.toMutableList()
