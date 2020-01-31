54405285e6884d0421f133cf9a4e95c164423ffc
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-01-31 12:28:22 +0330

    +test codes to put audio data into image in activity

--- app/src/main/java/ir/mrahimy/conceal/MainActivity.kt
+++ app/src/main/java/ir/mrahimy/conceal/MainActivity.kt
@@ -8,15 +8,13 @@ import com.github.squti.androidwaverecorder.WaveRecorder
 import ir.mrahimy.conceal.data.WavUtil.fromWaveData
 import ir.mrahimy.conceal.data.mapToRgbValue
 import ir.mrahimy.conceal.data.mapToUniformDouble
-import ir.mrahimy.conceal.util.Wave
-import ir.mrahimy.conceal.util.getRgbArray
-import ir.mrahimy.conceal.util.remove3Lsb
-import ir.mrahimy.conceal.util.toSeparatedDigits
+import ir.mrahimy.conceal.util.*
 import kotlinx.android.synthetic.main.activity_main.*
 import permissions.dispatcher.NeedsPermission
 import permissions.dispatcher.RuntimePermissions
 import java.io.File
 import java.util.*
+import kotlin.math.absoluteValue
 
 
 @RuntimePermissions
@@ -33,24 +31,172 @@ class MainActivity : AppCompatActivity() {
         askPermissions()
         val waveFile = fromWaveData(
             Wave.WavFile.openWavFile(
-                File(
-                    "/storage/emulated/0/Download/8k16bitpcm.wav"
-                )
+                File("/storage/emulated/0/Download/8k16bitpcm.wav")
             )
         )
 
-        val doubled = waveFile.data.mapToUniformDouble()
-        val rgb = doubled.mapToRgbValue()
+        val audioDataAsRgbList = waveFile.data.mapToUniformDouble().mapToRgbValue()
 
-        val image = BitmapFactory.decodeFile(
-            "/storage/emulated/0/Download/deer.jpg"
-        )
+        val image = BitmapFactory.decodeFile("/storage/emulated/0/Download/deer.jpg")
 
-        val _rgbList = image.getRgbArray()
+        val rgbList = image.getRgbArray().remove3Lsb()
 
-        val rgbList = _rgbList.remove3Lsb()
+        val audioSampleRate = waveFile.sampleRate.toString().toSeparatedDigits()
 
-        val rate = waveFile.sampleRate.toString().toSeparatedDigits()
+        val sampleRateElementCount = audioSampleRate.elementCount.toBinString(format = "%4s")
+
+        var position = 0
+
+        var binaryString2BitsChunk = sampleRateElementCount.substring(0, 2).toInt()
+        rgbList[position].r = rgbList[position].r.bitwiseOr(binaryString2BitsChunk)
+        position += 1
+
+        binaryString2BitsChunk = sampleRateElementCount.substring(2, 4).toInt()
+        rgbList[position].r = rgbList[position].r.bitwiseOr(binaryString2BitsChunk)
+        position += 1
+
+        repeat(audioSampleRate.elementCount) {
+            val element = it.toBinString(format = "%4s")
+            binaryString2BitsChunk = element.substring(0, 2).toInt()
+            rgbList[position].r = rgbList[position].r.bitwiseOr(binaryString2BitsChunk)
+            position += 1
+
+            binaryString2BitsChunk = element.substring(2, 4).toInt()
+            rgbList[position].r = rgbList[position].r.bitwiseOr(binaryString2BitsChunk)
+            position += 1
+        }
+
+        var shouldChangeTheLayer = false
+        var lastIndexOfWaveDataChecked = 0
+        var dataExceedsTheContainer = false
+
+        audioDataAsRgbList.forEachIndexed { index, item ->
+            if (position + 3 > (image.width - 1) * (image.height - 1)) {
+                shouldChangeTheLayer = true
+                /** breaks this for each*/
+                return
+            }
+
+            val data = if (item < 0) {
+                val negativeHandled = rgbList[position].r.bitwiseOr(4)
+                rgbList[position].r = negativeHandled
+                item.absoluteValue
+            } else item
+
+            var element = data.toBinString()
+            binaryString2BitsChunk = element.substring(0, 2).toInt()
+            rgbList[position].r = rgbList[position].r.bitwiseOr(binaryString2BitsChunk)
+            position += 1
+
+            element = data.toBinString()
+            binaryString2BitsChunk = element.substring(2, 4).toInt()
+            rgbList[position].r = rgbList[position].r.bitwiseOr(binaryString2BitsChunk)
+            position += 1
+
+            element = data.toBinString()
+            binaryString2BitsChunk = element.substring(4, 6).toInt()
+            rgbList[position].r = rgbList[position].r.bitwiseOr(binaryString2BitsChunk)
+            position += 1
+
+            element = data.toBinString()
+            binaryString2BitsChunk = element.substring(6, 8).toInt()
+            rgbList[position].r = rgbList[position].r.bitwiseOr(binaryString2BitsChunk)
+            position += 1
+
+            lastIndexOfWaveDataChecked = index
+        }
+
+        if (shouldChangeTheLayer) {
+            shouldChangeTheLayer = false
+            position = 0
+
+            audioDataAsRgbList.forEachIndexed { index, item ->
+                if (index < lastIndexOfWaveDataChecked) {
+                    /** continues this forEach to the next element */
+                    return@forEachIndexed
+                }
+
+                if (position + 3 > (image.width - 1) * (image.height - 1)) {
+                    shouldChangeTheLayer = true
+                    /** breaks this for each*/
+                    return
+                }
+
+                val data = if (item < 0) {
+                    val negativeHandled = rgbList[position].g.bitwiseOr(4)
+                    rgbList[position].g = negativeHandled
+                    item.absoluteValue
+                } else item
+
+                var element = data.toBinString()
+                binaryString2BitsChunk = element.substring(0, 2).toInt()
+                rgbList[position].g = rgbList[position].g.bitwiseOr(binaryString2BitsChunk)
+                position += 1
+
+                element = data.toBinString()
+                binaryString2BitsChunk = element.substring(2, 4).toInt()
+                rgbList[position].g = rgbList[position].g.bitwiseOr(binaryString2BitsChunk)
+                position += 1
+
+                element = data.toBinString()
+                binaryString2BitsChunk = element.substring(4, 6).toInt()
+                rgbList[position].g = rgbList[position].g.bitwiseOr(binaryString2BitsChunk)
+                position += 1
+
+                element = data.toBinString()
+                binaryString2BitsChunk = element.substring(6, 8).toInt()
+                rgbList[position].g = rgbList[position].g.bitwiseOr(binaryString2BitsChunk)
+                position += 1
+
+                lastIndexOfWaveDataChecked = index
+            }
+        }
+
+        if (shouldChangeTheLayer) {
+            shouldChangeTheLayer = false
+            position = 0
+
+            audioDataAsRgbList.forEachIndexed { index, item ->
+                if (index < lastIndexOfWaveDataChecked) {
+                    /** continues this forEach to the next element */
+                    return@forEachIndexed
+                }
+
+                if (position + 3 > (image.width - 1) * (image.height - 1)) {
+                    dataExceedsTheContainer = true
+                    /** breaks this for each*/
+                    return
+                }
+
+                val data = if (item < 0) {
+                    val negativeHandled = rgbList[position].b.bitwiseOr(4)
+                    rgbList[position].b = negativeHandled
+                    item.absoluteValue
+                } else item
+
+                var element = data.toBinString()
+                binaryString2BitsChunk = element.substring(0, 2).toInt()
+                rgbList[position].b = rgbList[position].b.bitwiseOr(binaryString2BitsChunk)
+                position += 1
+
+                element = data.toBinString()
+                binaryString2BitsChunk = element.substring(2, 4).toInt()
+                rgbList[position].b = rgbList[position].b.bitwiseOr(binaryString2BitsChunk)
+                position += 1
+
+                element = data.toBinString()
+                binaryString2BitsChunk = element.substring(4, 6).toInt()
+                rgbList[position].b = rgbList[position].b.bitwiseOr(binaryString2BitsChunk)
+                position += 1
+
+                element = data.toBinString()
+                binaryString2BitsChunk = element.substring(6, 8).toInt()
+                rgbList[position].b = rgbList[position].b.bitwiseOr(binaryString2BitsChunk)
+                position += 1
+
+                lastIndexOfWaveDataChecked = index
+            }
+        }
 
         record?.setOnClickListener {
             startRecording()
