8faea95859f13a6698c564f233e3ccd09029da54
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-01-31 14:37:57 +0330

    reversing w and h to match my mind's mapping

--- app/src/main/java/ir/mrahimy/conceal/util/Bitmap.kt
+++ app/src/main/java/ir/mrahimy/conceal/util/Bitmap.kt
@@ -2,12 +2,12 @@ package ir.mrahimy.conceal.util
 
 import android.graphics.Bitmap
 
-fun Bitmap.getRgb(x: Int, y: Int) : Rgb = this.getPixel(x, y).toRgb()
+fun Bitmap.getRgb(x: Int, y: Int): Rgb = this.getPixel(x, y).toRgb()
 
 fun Bitmap.getRgbArray(): List<Rgb> {
     val rgb = mutableListOf<Rgb>()
-    (0 until width).forEach { x ->
-        (0 until height).forEach { y ->
+    (0 until height).forEach { y ->
+        (0 until width).forEach { x ->
             rgb.add(getRgb(x, y))
         }
     }
