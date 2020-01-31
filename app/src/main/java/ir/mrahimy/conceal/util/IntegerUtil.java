d5b65e62cac024bfc03351b9ac93511360c57e98
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-01-31 12:25:28 +0330

    +and, +or

--- app/src/main/java/ir/mrahimy/conceal/util/IntegerUtil.java
+++ app/src/main/java/ir/mrahimy/conceal/util/IntegerUtil.java
@@ -4,4 +4,11 @@ public class IntegerUtil {
     static int removeLsBits(int in){
         return in & 248;
     }
+
+    static int and(int in1, int in2){
+        return in1 & in2;
+    }
+    static int or(int in1, int in2){
+        return in1 | in2;
+    }
 }
