ce90ade39e35b3d0a1f7af49aecea1e1aeb7589c
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-01-31 12:26:03 +0330

    +and, +or, +toBinary

--- app/src/main/java/ir/mrahimy/conceal/util/Rgb.kt
+++ app/src/main/java/ir/mrahimy/conceal/util/Rgb.kt
@@ -1,11 +1,20 @@
 package ir.mrahimy.conceal.util
 
 data class Rgb(
-    val r: Int,
-    val g: Int,
-    val b: Int
+    var r: Int,
+    var g: Int,
+    var b: Int
 )
 
 fun Int.toRgb() = Image.getRgb(this)
 
 fun Int.removeLsBits(howMany: Int) = IntegerUtil.removeLsBits(this)
+
+fun Int.bitwiseAnd(other: Int) = IntegerUtil.and(this, other)
+
+fun Int.bitwiseOr(other: Int) = IntegerUtil.or(this, other)
+
+fun Int.toBinString(format: String = "%8s") =
+    String.format(format, this.toString(2))
+        .replace(' ', '0')
+//    Integer.toBinaryString(this)
