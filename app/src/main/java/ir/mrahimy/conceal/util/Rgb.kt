a86bd5b3df66713f0c182b90ccf8c79e75b20c24
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-01 19:50:50 +0330

    +method to combine bits of integers

--- app/src/main/java/ir/mrahimy/conceal/util/Rgb.kt
+++ app/src/main/java/ir/mrahimy/conceal/util/Rgb.kt
@@ -1,5 +1,7 @@
 package ir.mrahimy.conceal.util
 
+import java.lang.RuntimeException
+
 data class Rgb(
     var r: Int,
     var g: Int,
@@ -9,7 +11,26 @@ data class Rgb(
 fun Int.toRgb() = Image.getRgb(this)
 fun Rgb.parse() = Image.parseRgb(this)
 
+//TODO: decide by howMany here
 fun Int.removeLsBits(howMany: Int) = IntegerUtil.removeLsBits(this)
+
+fun Int.getLsBits(howMany: Int) = when (howMany) {
+    3 -> IntegerUtil.get3LsBits(this)
+    2 -> IntegerUtil.get2LsBits(this)
+    else -> throw RuntimeException("Not implemented YET")
+}
+
+/**
+ * This method does not respect the sign of concealed number
+ */
+fun Int.combineBits(other: Int): Int {
+    val binA = this.toBinString()
+    val binB = this.toBinString()
+    val lsbA = binA.drop(6)
+    val lsbB = binB.drop(6)
+    val whole = lsbA + lsbB
+    return whole.toInt(2)
+}
 
 fun Int.bitwiseAnd(other: Int) = IntegerUtil.and(this, other)
 
