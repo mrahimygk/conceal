065a53b42a2ee3a69011833172bcc89a45f8896b
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-01 20:18:08 +0330

    +test for removing lsb

--- app/src/test/java/ir/mrahimy/conceal/ImageManipulationUnitTest.kt
+++ app/src/test/java/ir/mrahimy/conceal/ImageManipulationUnitTest.kt
@@ -1,19 +1,40 @@
 package ir.mrahimy.conceal
 
-import android.graphics.Color
-import ir.mrahimy.conceal.util.parse
-import ir.mrahimy.conceal.util.toRgb
+import ir.mrahimy.conceal.util.Rgb
+import ir.mrahimy.conceal.util.remove3Lsb
 import org.junit.Test
 
 class ImageManipulationUnitTest {
 
+    private val rgbList = mutableListOf<Rgb>().apply {
+        add(Rgb(192, 117, 115))
+        add(Rgb(180, 215, 216))
+        add(Rgb(181, 25, 26))
+        add(Rgb(81, 250, 16))
+        add(Rgb(50, 200, 19))
+        add(Rgb(150, 200, 190))
+        add(Rgb(90, 51, 17))
+        add(Rgb(17, 90, 51))
+        add(Rgb(170, 190, 151))
+    }
+
+    private val removedLsb = rgbList.remove3Lsb()
+
+    private fun `test removing 3 lsb of index`(index: Int, vararg intArray: Int) {
+        assert(removedLsb[index].r == intArray[0]) //0
+        assert(removedLsb[index].g == intArray[1])
+        assert(removedLsb[index].b == intArray[2])
+    }
 
     @Test
-    fun `convert to rgb and parse back`() {
-        val pixel = -4013374
-        val rgb = pixel.toRgb()
-        println("red: ${rgb.r}, green ${rgb.g}, blue ${rgb.b}")
-        val fromRgb = rgb.parse()
-        assert(pixel == fromRgb)
+    fun `test removing 3 lsb`() {
+        var index = 0
+        `test removing 3 lsb of index`(index++, 192, 112, 112) //0
+        `test removing 3 lsb of index`(8, 168, 184, 144) //8
+    }
+
+    @Test
+    fun `test putting sampleRate after removing LSB`() {
+
     }
 }
