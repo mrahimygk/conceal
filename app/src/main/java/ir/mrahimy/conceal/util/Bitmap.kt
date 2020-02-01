0f847ccc20dbd77710ff79b7b57b8cb16272fb8a
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-01 20:17:49 +0330

    +WIP get sample rate for rgb

--- app/src/main/java/ir/mrahimy/conceal/util/Bitmap.kt
+++ app/src/main/java/ir/mrahimy/conceal/util/Bitmap.kt
@@ -20,3 +20,23 @@ fun List<Rgb>.remove3Lsb(): List<Rgb> = map {
     val b = it.b.removeLsBits(3)
     Rgb(r, g, b)
 }.toMutableList()
+
+fun List<Rgb>.getSampleRate(): Int {
+    var position = 0
+    val digitCountFirst = get(position++).r.getLsBits(2)
+    val digitCountSecond = get(position++).r.getLsBits(2)
+
+    val digitCount = digitCountFirst.combineBits(digitCountSecond)
+    var digitList = mutableListOf<Int>()
+    repeat(digitCount) {
+        val first = get(position++).r.getLsBits(2)
+        val second = get(position++).r.getLsBits(2)
+        digitList.add(first.combineBits(second))
+    }
+
+    return digitList.joinToString().toInt()
+//    val r = it.r.removeLsBits(3)
+//    val g = it.g.removeLsBits(3)
+//    val b = it.b.removeLsBits(3)
+//    Rgb(r, g, b)
+}
