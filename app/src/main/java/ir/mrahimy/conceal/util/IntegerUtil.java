b5c8d5dd491fb59f2dcf20922483a98ba42b7808
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-01 19:49:53 +0330

    +methods to get ranged lsb of an integer

--- app/src/main/java/ir/mrahimy/conceal/util/IntegerUtil.java
+++ app/src/main/java/ir/mrahimy/conceal/util/IntegerUtil.java
@@ -1,14 +1,23 @@
 package ir.mrahimy.conceal.util;
 
 public class IntegerUtil {
-    static int removeLsBits(int in){
+    public static int removeLsBits(int in) {
         return in & 248;
     }
 
-    static int and(int in1, int in2){
+    public static int get2LsBits(int in) {
+        return in & 3;
+    }
+
+    public static int get3LsBits(int in) {
+        return in & 7;
+    }
+
+    public static int and(int in1, int in2) {
         return in1 & in2;
     }
-    static int or(int in1, int in2){
+
+    public static int or(int in1, int in2) {
         return in1 | in2;
     }
 }
