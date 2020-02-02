6cee9319868f7ae30ac1cb70fae7ec2a51789db0
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-02 21:45:44 +0330

    +method to put sample rate

--- app/src/main/java/ir/mrahimy/conceal/util/Bitmap.kt
+++ app/src/main/java/ir/mrahimy/conceal/util/Bitmap.kt
@@ -21,6 +21,45 @@ fun List<Rgb>.remove3Lsb(): List<Rgb> = map {
     Rgb(r, g, b)
 }.toMutableList()
 
+/**
+ * @param sampleRate the sample rate of audio to put inside lsb of the r layer
+ * @returns the position of last injected bit. maybe used to start inserting audio data
+ * (starting with that position itself)
+ */
+fun List<Rgb>.putSampleRate(sampleRate: Int) : Int {
+    val audioSampleRate = sampleRate.toString().toSeparatedDigits()
+    val sampleRateElementCount = audioSampleRate.elementCount.toBinString(format = "%4s")
+
+    var position = 0
+
+    var binaryString2BitsChunkStr = sampleRateElementCount.substring(0, 2)
+    var binaryString2BitsChunk = binaryString2BitsChunkStr.toInt(2)
+
+    var data = this[position].r.bitwiseOr(binaryString2BitsChunk)
+    this[position].r = data
+    position += 1
+
+    binaryString2BitsChunkStr = sampleRateElementCount.substring(2, 4)
+    binaryString2BitsChunk = binaryString2BitsChunkStr.toInt(2)
+
+    data = this[position].r.bitwiseOr(binaryString2BitsChunk)
+    this[position].r = data
+    position += 1
+
+    repeat(audioSampleRate.elementCount) {
+        val element = it.toBinString(format = "%4s")
+        binaryString2BitsChunk = element.substring(0, 2).toInt(2)
+        this[position].r = this[position].r.bitwiseOr(binaryString2BitsChunk)
+        position += 1
+
+        binaryString2BitsChunk = element.substring(2, 4).toInt(2)
+        this[position].r = this[position].r.bitwiseOr(binaryString2BitsChunk)
+        position += 1
+    }
+
+    return position
+}
+
 fun List<Rgb>.getSampleRate(): Int {
     var position = 0
     val digitCountFirst = get(position++).r.getLsBits(2)
