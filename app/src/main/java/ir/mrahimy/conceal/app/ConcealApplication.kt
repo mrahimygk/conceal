a16fe4035ce3b0bba27de17ff2062792c7bc85ce
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-05 21:19:08 +0330

    +app

---
+++ app/src/main/java/ir/mrahimy/conceal/app/ConcealApplication.kt
@@ -0,0 +1,9 @@
+package ir.mrahimy.conceal.app
+
+import android.app.Activity
+import android.app.Application
+
+class ConcealApplication :Application(){
+    var currentActivity: Activity? = null
+
+}
