8cf00741c1ca04c1c05450dec85c379284a45943
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-05 21:19:37 +0330

    +base android vmodel

---
+++ app/src/main/java/ir/mrahimy/conceal/base/BaseAndroidViewModel.kt
@@ -0,0 +1,29 @@
+package ir.mrahimy.conceal.base
+
+import android.app.Application
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorRes
+import androidx.annotation.StringRes
+import ir.mrahimy.conceal.app.ConcealApplication
+import ir.mrahimy.conceal.util.ktx.getColorCompat
+import ir.mrahimy.conceal.util.ktx.getColorCompatFromAttr
+
+class BaseAndroidViewModel(
+    private val application: Application,
+    private val model: BaseModel) : BaseViewModel(model) {
+
+    protected fun getApplication() = application as ConcealApplication
+
+
+    protected fun getString(@StringRes resId: Int, vararg formatArgs: Any?): String {
+        return getApplication().resources.getString(resId, *formatArgs)
+    }
+
+    protected fun getColorFromAttr(@AttrRes resId: Int): Int {
+        return getApplication().applicationContext.getColorCompatFromAttr(resId)
+    }
+
+    protected fun getColor(@ColorRes resId: Int): Int {
+        return getApplication().applicationContext.getColorCompat(resId)
+    }
+}
