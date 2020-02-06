a1cff91114b65f3fc68c7cfa71268843841d57df
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-07 00:54:23 +0330

    +MainModel to di

--- app/src/main/java/ir/mrahimy/conceal/di/ModelModule.kt
+++ app/src/main/java/ir/mrahimy/conceal/di/ModelModule.kt
@@ -1,8 +1,10 @@
 package ir.mrahimy.conceal.di
 
+import ir.mrahimy.conceal.ui.home.MainActivityModel
 import ir.mrahimy.conceal.ui.sample.SampleModel
 import org.koin.dsl.module
 
 val modelModule = module {
     factory { SampleModel() }
+    factory { MainActivityModel() }
 }
