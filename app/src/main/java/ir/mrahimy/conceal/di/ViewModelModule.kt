2e5cd79b4c962a4ad3aa8f80f4d26d8740db3def
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-07 00:54:36 +0330

    +mainVModel to di

--- app/src/main/java/ir/mrahimy/conceal/di/ViewModelModule.kt
+++ app/src/main/java/ir/mrahimy/conceal/di/ViewModelModule.kt
@@ -1,9 +1,12 @@
 package ir.mrahimy.conceal.di
 
+import ir.mrahimy.conceal.ui.home.MainActivityViewModel
 import ir.mrahimy.conceal.ui.sample.SampleViewModel
+import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { SampleViewModel(get()) }
+    viewModel { MainActivityViewModel(androidApplication(), get()) }
 }
