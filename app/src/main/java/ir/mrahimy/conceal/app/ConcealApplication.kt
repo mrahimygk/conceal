9ee3d8a6a78908cc468e3dd9d6d22aa6dc8e416a
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-06 20:02:17 +0330

    initing koin

--- app/src/main/java/ir/mrahimy/conceal/app/ConcealApplication.kt
+++ app/src/main/java/ir/mrahimy/conceal/app/ConcealApplication.kt
@@ -2,8 +2,26 @@ package ir.mrahimy.conceal.app
 
 import android.app.Activity
 import android.app.Application
+import ir.mrahimy.conceal.di.adapterModule
+import ir.mrahimy.conceal.di.modelModule
+import ir.mrahimy.conceal.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
-class ConcealApplication :Application(){
+class ConcealApplication : Application() {
     var currentActivity: Activity? = null
 
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@ConcealApplication)
+            androidLogger(Level.DEBUG)
+            modules(
+                adapterModule, viewModelModule, modelModule
+            )
+        }
+    }
 }
