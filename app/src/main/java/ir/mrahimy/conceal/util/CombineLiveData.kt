173d5646fe5e9743a291d773bf6a1d769d8a2662
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-06 22:30:05 +0330

    +combine 3 livedata

--- app/src/main/java/ir/mrahimy/conceal/util/CombineLiveData.kt
+++ app/src/main/java/ir/mrahimy/conceal/util/CombineLiveData.kt
@@ -24,3 +24,31 @@ inline fun <T1, T2, R> combine(
 
 }
 
+inline fun <T1, T2, T3, R> combine(
+    source1: LiveData<T1>,
+    source2: LiveData<T2>,
+    source3: LiveData<T3>,
+    crossinline combine: (data1: T1?, data2: T2?, data3: T3?) -> R
+): LiveData<R> = MediatorLiveData<R>().apply {
+
+    var data1: T1? = null
+    var data2: T2? = null
+    var data3: T3? = null
+
+    addSource(source1) {
+        data1 = it
+        value = combine(data1, data2, data3)
+    }
+
+    addSource(source2) {
+        data2 = it
+        value = combine(data1, data2, data3)
+    }
+
+    addSource(source3) {
+        data3 = it
+        value = combine(data1, data2, data3)
+    }
+
+}
+
