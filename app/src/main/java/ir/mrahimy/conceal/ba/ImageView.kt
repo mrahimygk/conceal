fd4a444006491d35093877f9409d06fbcf425b44
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-07 13:27:30 +0330

    +ba for getting drawables

--- app/src/main/java/ir/mrahimy/conceal/ba/ImageView.kt
+++ app/src/main/java/ir/mrahimy/conceal/ba/ImageView.kt
@@ -2,9 +2,18 @@ package ir.mrahimy.conceal.ba
 
 import android.graphics.Bitmap
 import android.widget.ImageView
+import androidx.annotation.DrawableRes
 import androidx.databinding.BindingAdapter
+import ir.mrahimy.conceal.util.ktx.getDrawableCompat
 
 @BindingAdapter("bitmap")
 fun ImageView.setBitmap(bitmap: Bitmap?) = bitmap?.let {
     setImageBitmap(bitmap)
+}
+
+@BindingAdapter("drawableCompat")
+fun ImageView.setDrawableCompat(@DrawableRes resId: Int?) {
+    resId?.let {
+        setImageDrawable(context.getDrawableCompat(it))
+    }
 }
