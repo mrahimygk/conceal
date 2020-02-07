1aea130ca3ae28e876d3390d2a933876002f7ac5
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-07 23:20:57 +0330

    +recordings adapter to injections

--- app/src/main/java/ir/mrahimy/conceal/di/AdapterModule.kt
+++ app/src/main/java/ir/mrahimy/conceal/di/AdapterModule.kt
@@ -1,8 +1,10 @@
 package ir.mrahimy.conceal.di
 
+import ir.mrahimy.conceal.ui.home.RecordingsAdapter
 import ir.mrahimy.conceal.ui.sample.SampleAdapter
 import org.koin.dsl.module
 
 val adapterModule = module {
     factory { SampleAdapter() }
+    factory { RecordingsAdapter() }
 }
