864c181f4600db2b18123a0e791032d44168a59b
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-07 15:17:18 +0330

    +ba to show tooltip

--- app/src/main/java/ir/mrahimy/conceal/ba/View.kt
+++ app/src/main/java/ir/mrahimy/conceal/ba/View.kt
@@ -2,6 +2,10 @@ package ir.mrahimy.conceal.ba
 
 import android.view.View
 import androidx.databinding.BindingAdapter
+import android.view.Gravity
+import io.github.douglasjunior.androidSimpleTooltip.SimpleTooltip
+import ir.mrahimy.conceal.R
+
 
 @BindingAdapter("app:isVisible")
 fun View.setIsVisible(boolean: Boolean?) {
@@ -19,3 +23,18 @@ fun View.setIsGone(boolean: Boolean?) {
     }
 }
 
+@BindingAdapter("app:tooltip")
+fun View.setTooltip(hasTooltip: Boolean?) {
+    val tooltip = SimpleTooltip.Builder(this.context)
+        .anchorView(this)
+        .text(context.getString(R.string.hold_to_start_recording_tooltip))
+        .gravity(Gravity.TOP)
+        .animated(true)
+        .transparentOverlay(false)
+        .build()
+
+    hasTooltip?.let {
+        if (it) tooltip.show()
+        else tooltip.dismiss()
+    }
+}
