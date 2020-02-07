6f98c4786bb437a3b519d4e638035118d6e86d3d
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-07 17:35:46 +0330

    +uri

--- app/src/main/java/ir/mrahimy/conceal/data/Recording.kt
+++ app/src/main/java/ir/mrahimy/conceal/data/Recording.kt
@@ -1,9 +1,11 @@
 package ir.mrahimy.conceal.data
 
+import android.net.Uri
 import java.util.*
 
 data class Recording(
     val id: Int,
     val file: String,
+    val uri: Uri,
     val date: Date
 )
