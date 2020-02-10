fc3cf54e1f65010e64325dc76182664956e8a443
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-10 20:36:01 +0330

    +job to input conceal capsule

--- app/src/main/java/ir/mrahimy/conceal/data/capsules/ConcealInputData.kt
+++ app/src/main/java/ir/mrahimy/conceal/data/capsules/ConcealInputData.kt
@@ -2,10 +2,12 @@ package ir.mrahimy.conceal.data.capsules
 
 import android.graphics.Bitmap
 import ir.mrahimy.conceal.data.Rgb
+import kotlinx.coroutines.Job
 
 data class ConcealInputData(
     val rgbList: List<Rgb>,
     val position: Int,
     val audioDataAsRgbList: IntArray,
-    val refImage: Bitmap
+    val refImage: Bitmap,
+    val job: Job
 )
