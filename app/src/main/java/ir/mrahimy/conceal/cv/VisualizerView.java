fa3f95ce8e04b90a201e578775f8743d8e84ef1b
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-12 21:36:24 +0330

    removing unused parts

--- app/src/main/java/ir/mrahimy/conceal/cv/VisualizerView.java
+++ app/src/main/java/ir/mrahimy/conceal/cv/VisualizerView.java
@@ -5,7 +5,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 import java.util.Random;
@@ -13,13 +12,6 @@ import java.util.Random;
 public class VisualizerView extends View {
     private static final int MAX_AMPLITUDE = 32767;
 
-    private float[] amplitudes;
-    private float[] vectors;
-    private int insertIdx = 0;
-    private Paint pointPaint;
-    private Paint linePaint;
-    private int width;
-    private int height;
     private int mPoints = 66;
     private int mRadius;
     private int mPointRadius;
@@ -31,13 +23,6 @@ public class VisualizerView extends View {
 
     public VisualizerView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        linePaint = new Paint();
-        linePaint.setColor(Color.GREEN);
-        linePaint.setStrokeWidth(1);
-        pointPaint = new Paint();
-        pointPaint.setColor(Color.BLUE);
-        pointPaint.setStrokeWidth(1);
-
         mPaint = new Paint();
         mPaint.setColor(Color.parseColor("#2196F3"));
         mPaint.setStrokeWidth(2);
@@ -54,10 +39,6 @@ public class VisualizerView extends View {
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        this.width = w;
-        height = h;
-        amplitudes = new float[this.width * 2]; // xy for each point across the width
-        vectors = new float[this.width * 4]; // xxyy for each line across the width
         mRadius = Math.min(w, h) / 8;
         mPointRadius = Math.abs((int) (2 * mRadius * Math.sin(Math.PI / mPoints / 3)));
     }
@@ -67,24 +48,11 @@ public class VisualizerView extends View {
      */
     public void addAmplitude(int amplitude) {
         invalidate();
-        float scaledHeight = ((float) amplitude / MAX_AMPLITUDE) * (height - 1);
-        int ampIdx = insertIdx * 2;
-        amplitudes[ampIdx++] = insertIdx;   // x
-        amplitudes[ampIdx] = scaledHeight;  // y
-        int vectorIdx = insertIdx * 4;
-        vectors[vectorIdx++] = insertIdx;   // x0
-        vectors[vectorIdx++] = 0;           // y0
-        vectors[vectorIdx++] = insertIdx;   // x1
-        vectors[vectorIdx] = scaledHeight;  // y1
-        // insert index must be shorter than screen width
-        insertIdx = ++insertIdx >= width ? 0 : insertIdx;
-
         float amp = amplitude / 10f;
         mSrcY = new float[mPoints];
         for (int i = 1; i <= mPoints; i++) {
             mSrcY[i - 1] = amp / (random.nextInt(thresholds[i - 1]) + 1);
         }
-        Log.d("TAG", "" + mSrcY[0]);
     }
 
     @Override
