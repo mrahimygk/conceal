e806798e42c3b4869655c2bec57b1c04bdc9ffd3
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-13 23:42:42 +0330

    +ktx to save file

--- app/src/main/java/ir/mrahimy/conceal/util/File.kt
+++ app/src/main/java/ir/mrahimy/conceal/util/File.kt
@@ -1,6 +1,7 @@
 package ir.mrahimy.conceal.util
 
 import android.graphics.Bitmap
+import ir.mrahimy.conceal.data.Waver
 import java.io.File
 
 fun File.writeBitmap(bitmap: Bitmap, format: Bitmap.CompressFormat, quality: Int = 100) {
@@ -8,4 +9,14 @@ fun File.writeBitmap(bitmap: Bitmap, format: Bitmap.CompressFormat, quality: Int
         bitmap.compress(format, quality, out)
         out.flush()
     }
+}
+
+fun File.writeWave(waver: Waver) {
+    Wave.WavFile.newWavFile(
+        this,
+        waver.channelCount,
+        waver.frameCount,
+        waver.validBits,
+        waver.sampleRate
+    )
 }
