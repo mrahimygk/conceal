af1dcf63758b0f8a4189c4736ed48b4d3fadac29
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-14 11:47:50 +0330

    +ktx to save file

--- app/src/main/java/ir/mrahimy/conceal/data/capsules/SaveWaveInfoCapsule.kt
+++ app/src/main/java/ir/mrahimy/conceal/data/capsules/SaveWaveInfoCapsule.kt
@@ -1,6 +1,9 @@
 package ir.mrahimy.conceal.data.capsules
 
 import ir.mrahimy.conceal.data.Waver
+import ir.mrahimy.conceal.util.ktx.toValidPath
+import ir.mrahimy.conceal.util.writeWave
+import java.io.File
 import java.util.*
 
 data class SaveWaveInfoCapsule(
@@ -8,3 +11,9 @@ data class SaveWaveInfoCapsule(
     val time: Date?,
     val data: Waver
 )
+
+fun SaveWaveInfoCapsule.save(path: String): String {
+    val filePath = path.toValidPath() + "${name}_${time?.time}.wav"
+    File(filePath).writeWave(data)
+    return path
+}
