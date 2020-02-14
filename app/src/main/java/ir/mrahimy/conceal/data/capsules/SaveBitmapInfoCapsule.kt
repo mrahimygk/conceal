eaacb6112401a9fa1d30895e07093d322ad5f4d0
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-14 11:47:40 +0330

    +ktx to save file

--- app/src/main/java/ir/mrahimy/conceal/data/capsules/SaveBitmapInfoCapsule.kt
+++ app/src/main/java/ir/mrahimy/conceal/data/capsules/SaveBitmapInfoCapsule.kt
@@ -1,6 +1,9 @@
 package ir.mrahimy.conceal.data.capsules
 
 import android.graphics.Bitmap
+import ir.mrahimy.conceal.util.ktx.toValidPath
+import ir.mrahimy.conceal.util.writeBitmap
+import java.io.File
 import java.util.*
 
 data class SaveBitmapInfoCapsule(
@@ -9,3 +12,10 @@ data class SaveBitmapInfoCapsule(
     val bitmap: Bitmap,
     val format: Bitmap.CompressFormat
 )
+
+fun SaveBitmapInfoCapsule.save(path: String): String {
+    val filePath = path.toValidPath() + "${name}_${time?.time}." +
+            format.name.toLowerCase(Locale.ENGLISH)
+    File(filePath).writeBitmap(bitmap, format, 100)
+    return path
+}
