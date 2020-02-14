46136c15fd10d05b2f12c075602641c8afb651f2
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-14 11:35:05 +0330

    +migration sample to ref later

---
+++ app/src/main/java/ir/mrahimy/conceal/data/db/migrations/Migrations_1_10.kt
@@ -0,0 +1,10 @@
+package ir.mrahimy.conceal.data.db.migrations
+
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+val migration1to2 = object : Migration(1,2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("DROP TABLE IF EXISTS EMPTY")
+    }
+}
