d249c9e071b50cda921f1dca9bdd8ef3276f0848
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-14 12:24:01 +0330

    +getting file name

--- app/src/main/java/ir/mrahimy/conceal/util/ktx/String.kt
+++ app/src/main/java/ir/mrahimy/conceal/util/ktx/String.kt
@@ -1,5 +1,9 @@
 package ir.mrahimy.conceal.util.ktx
 
+import java.io.File
+
 fun String.toValidPath(): String {
     return if (this.endsWith('/')) this else "${this}/"
 }
+
+fun String.getNameFromPath() = File(this).name.split('.')[0]
