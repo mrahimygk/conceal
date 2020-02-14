f13b28e253522c818081ab0f6eea5c2cf32c40f0
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-14 11:34:20 +0330

    +capsule to save wave file info

---
+++ app/src/main/java/ir/mrahimy/conceal/data/capsules/SaveWaveInfoCapsule.kt
@@ -0,0 +1,10 @@
+package ir.mrahimy.conceal.data.capsules
+
+import ir.mrahimy.conceal.data.Waver
+import java.util.*
+
+data class SaveWaveInfoCapsule(
+    val name: String?,
+    val time: Date?,
+    val data: Waver
+)
