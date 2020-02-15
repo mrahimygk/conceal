9e163601158a1d55c7723e350ddbf8bf5d6b1352
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-15 22:14:28 +0330

    +ktx to parse waver

--- app/src/main/java/ir/mrahimy/conceal/util/ktx/Bitmap.kt
+++ app/src/main/java/ir/mrahimy/conceal/util/ktx/Bitmap.kt
@@ -2,6 +2,8 @@ package ir.mrahimy.conceal.util.ktx
 
 import android.graphics.Bitmap
 import ir.mrahimy.conceal.data.Rgb
+import ir.mrahimy.conceal.data.Waver
+import ir.mrahimy.conceal.util.*
 
 fun Bitmap.getRgb(x: Int, y: Int): Rgb = this.getPixel(x, y).toRgb()
 
@@ -13,4 +15,28 @@ fun Bitmap.getRgbArray(): List<Rgb> {
         }
     }
     return rgb
+}
+
+fun Bitmap.parseWaver(): Waver {
+    val list = getRgbArray()
+    val parsedSampleRate = list.getSampleRate()
+    val parsedChannelCount = list.getChannelCount(parsedSampleRate.position)
+    val parsedFrameCount = list.getFrameCount(parsedChannelCount.position)
+    val parsedValidBits = list.getValidBits(parsedFrameCount.position)
+    val parsedMaxValue = list.getMaxValue(parsedValidBits.position)
+
+    val parsedWaveData =
+        list.getAllSignedIntegers(parsedMaxValue.position)
+            .map { n -> n.toDouble() / 255.0 }
+            .map { n -> n * parsedMaxValue.number }
+            .map { n -> n.toLong() }
+            .toLongArray()
+
+    return Waver(
+        parsedWaveData,
+        parsedSampleRate.number.toLong(),
+        parsedChannelCount.number,
+        parsedFrameCount.number.toLong(),
+        parsedValidBits.number
+    )
 }
