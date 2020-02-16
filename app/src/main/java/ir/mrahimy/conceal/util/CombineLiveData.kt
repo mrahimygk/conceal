1d907f185a009ca4e4e4bf036c87fdd45c3f8510
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-16 22:55:58 +0330

    +more combination functions

--- app/src/main/java/ir/mrahimy/conceal/util/CombineLiveData.kt
+++ app/src/main/java/ir/mrahimy/conceal/util/CombineLiveData.kt
@@ -52,3 +52,81 @@ inline fun <T1, T2, T3, R> combine(
 
 }
 
+
+inline fun <T1, T2, T3, T4, R> combine(
+    source1: LiveData<T1>,
+    source2: LiveData<T2>,
+    source3: LiveData<T3>,
+    source4: LiveData<T4>,
+    crossinline combine: (data1: T1?, data2: T2?, data3: T3?, data4: T4?) -> R
+): LiveData<R> = MediatorLiveData<R>().apply {
+
+    var data1: T1? = null
+    var data2: T2? = null
+    var data3: T3? = null
+    var data4: T4? = null
+
+    addSource(source1) {
+        data1 = it
+        value = combine(data1, data2, data3, data4)
+    }
+
+    addSource(source2) {
+        data2 = it
+        value = combine(data1, data2, data3, data4)
+    }
+
+    addSource(source3) {
+        data3 = it
+        value = combine(data1, data2, data3, data4)
+    }
+
+    addSource(source4) {
+        data4 = it
+        value = combine(data1, data2, data3, data4)
+    }
+
+}
+
+
+inline fun <T1, T2, T3, T4, T5, R> combine(
+    source1: LiveData<T1>,
+    source2: LiveData<T2>,
+    source3: LiveData<T3>,
+    source4: LiveData<T4>,
+    source5: LiveData<T5>,
+    crossinline combine: (data1: T1?, data2: T2?, data3: T3?, data4: T4?, data5: T5?) -> R
+): LiveData<R> = MediatorLiveData<R>().apply {
+
+    var data1: T1? = null
+    var data2: T2? = null
+    var data3: T3? = null
+    var data4: T4? = null
+    var data5: T5? = null
+
+    addSource(source1) {
+        data1 = it
+        value = combine(data1, data2, data3, data4, data5)
+    }
+
+    addSource(source2) {
+        data2 = it
+        value = combine(data1, data2, data3, data4, data5)
+    }
+
+    addSource(source3) {
+        data3 = it
+        value = combine(data1, data2, data3, data4, data5)
+    }
+
+    addSource(source4) {
+        data4 = it
+        value = combine(data1, data2, data3, data4, data5)
+    }
+    addSource(source5) {
+        data5 = it
+        value = combine(data1, data2, data3, data4, data5)
+    }
+
+}
+
