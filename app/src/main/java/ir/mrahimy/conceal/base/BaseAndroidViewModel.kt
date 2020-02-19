57de51682988969044358ad96ad460a24ccd4bcf
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-20 00:45:04 +0330

    +fun to get dimens

--- app/src/main/java/ir/mrahimy/conceal/base/BaseAndroidViewModel.kt
+++ app/src/main/java/ir/mrahimy/conceal/base/BaseAndroidViewModel.kt
@@ -3,6 +3,7 @@ package ir.mrahimy.conceal.base
 import android.app.Application
 import androidx.annotation.AttrRes
 import androidx.annotation.ColorRes
+import androidx.annotation.DimenRes
 import androidx.annotation.StringRes
 import ir.mrahimy.conceal.app.ConcealApplication
 import ir.mrahimy.conceal.util.ktx.getColorCompat
@@ -18,6 +19,10 @@ abstract class BaseAndroidViewModel(
 
     protected fun getString(@StringRes resId: Int, vararg formatArgs: Any?): String {
         return getApplication().resources.getString(resId, *formatArgs)
+    }
+
+    protected fun getDimension(@DimenRes resId: Int): Float {
+        return getApplication().resources.getDimension(resId)
     }
 
     protected fun getColorFromAttr(@AttrRes resId: Int): Int {
