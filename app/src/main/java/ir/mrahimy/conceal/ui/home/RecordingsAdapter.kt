0cdcdf993530ed46495c980e6d89213858bed2a4
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-20 22:36:05 +0330

    +listeners on play buttons

--- app/src/main/java/ir/mrahimy/conceal/ui/home/RecordingsAdapter.kt
+++ app/src/main/java/ir/mrahimy/conceal/ui/home/RecordingsAdapter.kt
@@ -1,12 +1,17 @@
 package ir.mrahimy.conceal.ui.home
 
+import android.view.View
 import androidx.recyclerview.widget.DiffUtil
 import ir.mrahimy.conceal.R
 import ir.mrahimy.conceal.base.BaseAdapter
 import ir.mrahimy.conceal.data.Recording
-import ir.mrahimy.conceal.data.Sample
+import kotlinx.android.synthetic.main.item_recording.view.*
 
 class RecordingsAdapter : BaseAdapter<Recording>(DIFF_CALLBACK) {
+
+    var onStop: ((item: Recording, v: View) -> Unit)? = null
+    var onPlay: ((item: Recording, v: View) -> Unit)? = null
+    var onDelete: ((item: Recording, v: View) -> Unit)? = null
 
     companion object {
         private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Recording>() {
@@ -22,5 +27,18 @@ class RecordingsAdapter : BaseAdapter<Recording>(DIFF_CALLBACK) {
 
     override fun getItemViewType(position: Int): Int {
         return R.layout.item_recording
+    }
+
+    override fun onBindViewHolder(holder: DataBindingViewHolder, position: Int) {
+        super.onBindViewHolder(holder, position)
+        holder.itemView.stop?.setOnClickListener { v ->
+            onStop?.invoke(getItem(holder.adapterPosition), v)
+        }
+        holder.itemView.play?.setOnClickListener { v ->
+            onPlay?.invoke(getItem(holder.adapterPosition), v)
+        }
+        holder.itemView.delete?.setOnClickListener { v ->
+            onDelete?.invoke(getItem(holder.adapterPosition), v)
+        }
     }
 }
