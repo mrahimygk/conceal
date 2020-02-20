4daf503e963b1d23569a5c491805748ecc805b93
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-21 00:26:59 +0330

    +model to add recording

---
+++ app/src/main/java/ir/mrahimy/conceal/ui/parse/ParseActivityModel.kt
@@ -0,0 +1,9 @@
+package ir.mrahimy.conceal.ui.parse
+
+import ir.mrahimy.conceal.base.BaseModel
+import ir.mrahimy.conceal.data.Recording
+import ir.mrahimy.conceal.data.repository.RecordingRepository
+
+class ParseActivityModel(private val repository: RecordingRepository) : BaseModel() {
+    suspend fun addRecording(recording: Recording) = repository.addRecording(recording)
+}
