5fbf7f7d527f38a7d75ea8dde49e6feae2f97f7c
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-21 00:27:35 +0330

    +parse act model

--- app/src/main/java/ir/mrahimy/conceal/di/ModelModule.kt
+++ app/src/main/java/ir/mrahimy/conceal/di/ModelModule.kt
@@ -1,10 +1,12 @@
 package ir.mrahimy.conceal.di
 
 import ir.mrahimy.conceal.ui.home.MainActivityModel
+import ir.mrahimy.conceal.ui.parse.ParseActivityModel
 import ir.mrahimy.conceal.ui.sample.SampleModel
 import org.koin.dsl.module
 
 val modelModule = module {
     factory { SampleModel() }
     factory { MainActivityModel(get()) }
+    factory { ParseActivityModel(get()) }
 }
