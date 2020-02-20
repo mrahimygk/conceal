18f5c836968f264d601279aa50ab2c4a7adbd8fb
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-20 23:19:20 +0330

    +delete

--- app/src/main/java/ir/mrahimy/conceal/ui/home/MainActivityModel.kt
+++ app/src/main/java/ir/mrahimy/conceal/ui/home/MainActivityModel.kt
@@ -4,11 +4,11 @@ import ir.mrahimy.conceal.base.BaseModel
 import ir.mrahimy.conceal.data.Recording
 import ir.mrahimy.conceal.data.repository.RecordingRepository
 
-class MainActivityModel(private val repository: RecordingRepository): BaseModel(){
+class MainActivityModel(private val repository: RecordingRepository) : BaseModel() {
 
-    suspend fun addRecording(recording: Recording){
-        repository.addRecording(recording)
-    }
+    suspend fun addRecording(recording: Recording) = repository.addRecording(recording)
+
+    suspend fun deleteRecording(recording: Recording) = repository.deleteRecording(recording)
 
     fun getAllRecordings() = repository.getAllRecordings()
 }
