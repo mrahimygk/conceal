d2df6fa977f73f04cbd11ddddf2404614a20a53f
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-21 00:25:23 +0330

    +data class to have a reveal percentage

---
+++ app/src/main/java/ir/mrahimy/conceal/data/capsules/RevealPercentage.kt
@@ -0,0 +1,27 @@
+package ir.mrahimy.conceal.data.capsules
+
+import android.graphics.Bitmap
+import androidx.lifecycle.ViewModel
+import ir.mrahimy.conceal.data.Waver
+
+data class RevealPercentage(
+    val id: Int,
+    val percent: Float,
+    val bitmap: Bitmap?,
+    val waver: Waver?,
+    val positionOnList: Int,
+    val lastWaveArrayIndexChecked: Int,
+    val done: Boolean
+
+)
+
+fun ViewModel.emptyRevealPercentage() =
+    RevealPercentage(
+        1,
+        0f,
+        null,
+        null,
+        0,
+        0,
+        false
+    )
