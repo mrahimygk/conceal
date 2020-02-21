f7c6f2f0b6f1575be7521b3cc6fb79bd0e43ae4a
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-21 23:07:34 +0330

    +white color filter

--- app/src/main/java/ir/mrahimy/conceal/ba/FloatingActionButton.kt
+++ app/src/main/java/ir/mrahimy/conceal/ba/FloatingActionButton.kt
@@ -1,6 +1,5 @@
 package ir.mrahimy.conceal.ba
 
-import android.graphics.Color
 import android.graphics.PorterDuff
 import androidx.annotation.ColorRes
 import androidx.databinding.BindingAdapter
@@ -8,8 +7,10 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 @BindingAdapter("colorFilter")
 fun FloatingActionButton.setColorFilter(@ColorRes color: Int?) {
-    val myFabSrc = resources.getDrawable(android.R.drawable.ic_input_add)
-    val willBeWhite = myFabSrc.constantState!!.newDrawable()
-    willBeWhite.mutate().setColorFilter(Color.WHITE, PorterDuff.Mode.MULTIPLY)
-    setImageDrawable(willBeWhite)
+    color?.let { inColor ->
+        val myFabSrc = resources.getDrawable(android.R.drawable.ic_input_add)
+        val willBeWhite = myFabSrc.constantState!!.newDrawable()
+        willBeWhite.mutate().setColorFilter(inColor, PorterDuff.Mode.MULTIPLY)
+        setImageDrawable(willBeWhite)
+    }
 }
