9d436b14ddda8522b248bf609b88055fe6de6ed7
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-24 00:16:14 +0330

    +info Repo

--- app/src/main/java/ir/mrahimy/conceal/ui/home/MainActivityModel.kt
+++ app/src/main/java/ir/mrahimy/conceal/ui/home/MainActivityModel.kt
@@ -2,13 +2,26 @@ package ir.mrahimy.conceal.ui.home
 
 import ir.mrahimy.conceal.base.BaseModel
 import ir.mrahimy.conceal.data.Recording
+import ir.mrahimy.conceal.data.Waver
+import ir.mrahimy.conceal.net.req.makeAudioInfoMap
+import ir.mrahimy.conceal.repository.InfoRepository
 import ir.mrahimy.conceal.repository.RecordingRepository
+import java.io.File
 
-class MainActivityModel(private val repository: RecordingRepository) : BaseModel() {
+class MainActivityModel(
+    private val recordingRepository: RecordingRepository,
+    private val infoRepository: InfoRepository
+) : BaseModel() {
 
-    suspend fun addRecording(recording: Recording) = repository.addRecording(recording)
+    suspend fun addRecording(recording: Recording) = recordingRepository.addRecording(recording)
 
-    suspend fun deleteRecording(recording: Recording) = repository.deleteRecording(recording)
+    suspend fun deleteRecording(recording: Recording) =
+        recordingRepository.deleteRecording(recording)
 
-    fun getAllRecordings() = repository.getAllRecordings()
+    fun getAllRecordings() = recordingRepository.getAllRecordings()
+
+//    suspend fun putInputImageData() = infoRepository.putImageInfo(null)
+
+    suspend fun putInputWaveData(waver: Waver, file: File, isParsed: Boolean) =
+        infoRepository.putAudioInfo(waver.makeAudioInfoMap(isParsed, file))
 }
