91ca566a6aa97299178b04df7fa276fe6a8309ca
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-24 00:19:36 +0330

    +info repo

--- app/src/main/java/ir/mrahimy/conceal/di/RepositoryModule.kt
+++ app/src/main/java/ir/mrahimy/conceal/di/RepositoryModule.kt
@@ -1,9 +1,12 @@
 package ir.mrahimy.conceal.di
 
+import ir.mrahimy.conceal.repository.InfoRepository
+import ir.mrahimy.conceal.repository.InfoRepositoryImpl
 import ir.mrahimy.conceal.repository.RecordingRepository
 import ir.mrahimy.conceal.repository.RecordingRepositoryImpl
 import org.koin.dsl.module
 
 val repositoryModule = module {
     factory<RecordingRepository> { RecordingRepositoryImpl(get()) }
+    factory<InfoRepository> { InfoRepositoryImpl(get()) }
 }
