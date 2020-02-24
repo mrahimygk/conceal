551a745b8fefedc9b9e69fe5871fc868d8be47d2
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-24 22:32:40 +0330

    removing duplicated data

--- app/src/main/java/ir/mrahimy/conceal/ui/parse/ParseActivityModel.kt
+++ app/src/main/java/ir/mrahimy/conceal/ui/parse/ParseActivityModel.kt
@@ -1,9 +1,5 @@
 package ir.mrahimy.conceal.ui.parse
 
 import ir.mrahimy.conceal.base.BaseModel
-import ir.mrahimy.conceal.data.Recording
-import ir.mrahimy.conceal.repository.RecordingRepository
 
-class ParseActivityModel(private val repository: RecordingRepository) : BaseModel() {
-    suspend fun addRecording(recording: Recording) = repository.addRecording(recording)
-}
+class ParseActivityModel : BaseModel()
