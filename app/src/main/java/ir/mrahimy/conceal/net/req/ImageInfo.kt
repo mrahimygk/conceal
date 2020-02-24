80f2581ea7920fe63656532bcb40fda67df70449
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-24 22:06:28 +0330

    +ktx  to parse image

--- app/src/main/java/ir/mrahimy/conceal/net/req/ImageInfo.kt
+++ app/src/main/java/ir/mrahimy/conceal/net/req/ImageInfo.kt
@@ -1,5 +1,8 @@
 package ir.mrahimy.conceal.net.req
 
+import android.graphics.Bitmap
+import java.io.File
+
 private const val WIDTH_KEY = "width"
 private const val HEIGHT_KEY = "height"
 
@@ -18,5 +21,28 @@ internal fun makeImageInfoMap(
     date?.let { map.put(DATE_KEY, date.toString()) }
     w?.let { map.put(WIDTH_KEY, w.toString()) }
     h?.let { map.put(HEIGHT_KEY, h.toString()) }
+    return map
+}
+
+fun Bitmap.makeImageInfoMap(
+    isParsed: Boolean,
+    file: File
+): Map<String, String> {
+    val name = file.name
+    val ext = file.extension
+    val size = file.length().toString()
+    val date = file.lastModified()
+    val width = width
+    val height = height
+
+    val map = mutableMapOf<String, String>()
+    map[NAME_KEY] = name
+    map[EXT_KEY] = ext
+    map[SIZE_KEY] = size
+    map[DATE_KEY] = date.toString()
+
+    map[IS_PARSED_KEY] = isParsed.toString()
+    map[WIDTH_KEY] = width.toString()
+    map[HEIGHT_KEY] = height.toString()
     return map
 }
