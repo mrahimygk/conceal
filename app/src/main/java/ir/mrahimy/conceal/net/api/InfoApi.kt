5f5b7f9109b8bacf2dddcb47181a94775a498a1c
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-24 22:08:13 +0330

    fixing routes

--- app/src/main/java/ir/mrahimy/conceal/net/api/InfoApi.kt
+++ app/src/main/java/ir/mrahimy/conceal/net/api/InfoApi.kt
@@ -5,9 +5,9 @@ import retrofit2.http.QueryMap
 
 interface InfoApi {
 
-    @POST("image/")
+    @POST("image.php")
     suspend fun putImageInfo(@QueryMap params: Map<String, String>): Any
 
-    @POST("audio/")
+    @POST("audio.php")
     suspend fun putAudioInfo(@QueryMap params: Map<String, String>): Any
 }
