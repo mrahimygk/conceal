393af7756e6a7c8b8492dfc9e65fe3f5bfaee8e8
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-24 21:39:32 +0330

    +s

--- app/src/main/java/ir/mrahimy/conceal/net/BaseUrl.kt
+++ app/src/main/java/ir/mrahimy/conceal/net/BaseUrl.kt
@@ -3,5 +3,6 @@ package ir.mrahimy.conceal.net
 object BaseUrl {
     private const val VERSION = 1
 
-    const val BASE_URL = "http://conceal.ir/api/v$VERSION/"
+//    const val BASE_URL = "https://conceal.ir/api/v$VERSION/"
+    const val BASE_URL = "https://conceal.ir/"
 }
