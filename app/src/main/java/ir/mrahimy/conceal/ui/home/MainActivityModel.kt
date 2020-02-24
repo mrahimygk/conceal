440acffcb4c913336246bd77da923e4e0ff0f362
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-24 22:07:15 +0330

    +image

--- app/src/main/java/ir/mrahimy/conceal/ui/home/MainActivityModel.kt
+++ app/src/main/java/ir/mrahimy/conceal/ui/home/MainActivityModel.kt
@@ -1,9 +1,11 @@
 package ir.mrahimy.conceal.ui.home
 
+import android.graphics.Bitmap
 import ir.mrahimy.conceal.base.BaseModel
 import ir.mrahimy.conceal.data.Recording
 import ir.mrahimy.conceal.data.Waver
 import ir.mrahimy.conceal.net.req.makeAudioInfoMap
+import ir.mrahimy.conceal.net.req.makeImageInfoMap
 import ir.mrahimy.conceal.repository.InfoRepository
 import ir.mrahimy.conceal.repository.RecordingRepository
 import java.io.File
@@ -20,7 +22,11 @@ class MainActivityModel(
 
     fun getAllRecordings() = recordingRepository.getAllRecordings()
 
-//    suspend fun putInputImageData() = infoRepository.putImageInfo(null)
+    suspend fun putInputImageData(
+        bitmap: Bitmap,
+        file: File,
+        isParsed: Boolean
+    ) = infoRepository.putImageInfo(bitmap.makeImageInfoMap(isParsed, file))
 
     suspend fun putInputWaveData(waver: Waver, file: File, isParsed: Boolean) =
         infoRepository.putAudioInfo(waver.makeAudioInfoMap(isParsed, file))
