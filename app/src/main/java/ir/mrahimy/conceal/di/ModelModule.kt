93acf369bce57d9ccf0bd365480822e9a9817e2d
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-02-26 00:31:01 +0330

    injecting model

--- app/src/main/java/ir/mrahimy/conceal/di/ModelModule.kt
+++ app/src/main/java/ir/mrahimy/conceal/di/ModelModule.kt
@@ -3,10 +3,13 @@ package ir.mrahimy.conceal.di
 import ir.mrahimy.conceal.ui.home.MainActivityModel
 import ir.mrahimy.conceal.ui.parse.ParseActivityModel
 import ir.mrahimy.conceal.ui.sample.SampleModel
+import ir.mrahimy.conceal.ui.slide.SlideShowModel
 import org.koin.dsl.module
 
 val modelModule = module {
     factory { SampleModel() }
     factory { MainActivityModel(get(), get()) }
     factory { ParseActivityModel() }
+    factory { SlideShowModel() }
+
 }
