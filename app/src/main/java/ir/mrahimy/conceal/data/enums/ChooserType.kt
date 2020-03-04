b2ac5983db84d29a0c51fecfcdde1d5657f45ada
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-04 13:03:54 +0330

    restricting opening audio files to wave-only

--- app/src/main/java/ir/mrahimy/conceal/data/enums/ChooserType.kt
+++ app/src/main/java/ir/mrahimy/conceal/data/enums/ChooserType.kt
@@ -5,5 +5,5 @@ import android.provider.MediaStore
 
 enum class ChooserType(val typeString: String, val externalContentUri: Uri) {
     Image("image/*", MediaStore.Images.Media.EXTERNAL_CONTENT_URI),
-    Audio("audio/*", MediaStore.Audio.Media.EXTERNAL_CONTENT_URI),
+    Audio("audio/x-wav", MediaStore.Audio.Media.EXTERNAL_CONTENT_URI),
 }
